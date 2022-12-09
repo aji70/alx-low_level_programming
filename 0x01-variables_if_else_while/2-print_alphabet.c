bf611698f2ebc406c03cfc9fd9337e5d52e95eec
Ajidokwu Sabo <realjaiboi70@gmail.com>
2022-12-09 15:02:17 -0800

    variables

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib>
+#include <time.h>
+
+/**
+ * main - Entry
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char low;
+
+	for (low = 'a'; low <= 'z'; low++)
+		putchar(low);
+		putchar('\n');
+
+	return (0);
+}
