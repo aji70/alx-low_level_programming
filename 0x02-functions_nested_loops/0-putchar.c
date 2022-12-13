ea67783b2bbdd36dbf5777010e11d930191522c0
Ajidokwu Sabo <realjaiboi70@gmail.com>
2022-12-13 14:04:32 -0800

    0x02

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "_putchar.h"
+
+/**
+ * main - Entry
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	charword[8] = "_putchar";
+
+	int i;
+
+	for (i = 0; i < 8; i++)
+		_putchar(word[i]);
+	_putchar('\n');
+
+	return (0);
+}
