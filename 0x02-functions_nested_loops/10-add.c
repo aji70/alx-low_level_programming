69e2a8c552af7a7a99ee4dade36048470aa78e7f
Ajidokwu Sabo <realjaiboi70@gmail.com>
2022-12-14 14:15:21 -0800

    0x02

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - check main
+ * @a: An integer a
+ * @b: AN integer b
+ * Description: function that adds two integers and returns the result
+ * Return: Result to add a and b
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
---
+++ 0x02-functions_nested_loops/11-main.c
@@ -0,0 +1,17 @@
+#include "main"
+
+/**
+ * main - check the code for main
+ *
+ * Return: ALways 0
+ */
+
+int main(void)
+{
+	print_to_98(0);
+	print_to_98(98);
+	print_to_98(111);
+	print_to_98(81);
+	print_to_98(-10);
+	return (0);
+}
