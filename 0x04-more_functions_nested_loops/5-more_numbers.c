989468fc05480221d1fc7435269d3ddf65b9d0e1
Ajidokwu Sabo <realjaiboi70@gmail.com>
2022-12-15 10:35:53 -0800

    0x03

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -5,13 +5,14 @@
  * followed by a new line.
  * Return: ALways 0
  */
+
 void more_numbers(void)
 {
 	int i, j = 0
 
 		for (; j < 10; j++)
 		{
-			for (i = o; i <= 14; i++)
+			for (i = 0; i <= 14; i++)
 			{
 				if (i > 9)
 					_putchar(i / 10 + '0');
