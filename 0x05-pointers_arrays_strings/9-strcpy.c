ae36d07cff1dc7c2dec733c74c45a98b42f79dac
Ajidokwu Sabo <realjaiboi70@gmail.com>
2022-12-19 15:13:32 -0800

    0x05

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,19 @@
+/**
+ * _strcpy - a functiom that copied the string pointed to by src,
+ * including the terminating null byte (\0),
+ * to the buffer pointed to by dest.
+ *
+ * @dest: A pointer to destination of string
+ * @src: A pointer to the source of string to copy from
+ *
+ * Return: pointer to dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	char *aux = dest;
+
+	while (*src)
+		*dest++ = *src++;
+	return (aux);
+}
