3987fd1a0cd8f2f8fb7e12daeb5ef449bd2dc783
Ajidokwu Sabo <realjaiboi70@gmail.com>
2022-12-21 23:13:37 -0800

    0x06

--- 0x06-pointers_arrays_strings/2-main.c
+++ 0x06-pointers_arrays_strings/2-main.c
@@ -2,10 +2,11 @@
 #include <stdio.h>
 
 /**
- * main - check the code
+ * main -check the code
  *
- * Return: always 0
+ * Return: Always 0
  */
+
 int main(void)
 {
 	char s1[98];
@@ -24,6 +25,7 @@ int main(void)
 	ptr = _strncpy(s1, "First, solve the problem. Then, write the code\n", 90);
 	printf("%s", s1);
 	printf("%s", ptr);
+
 	for (i = 0; i < 98; i++)
 	{
 		if (i % 10)
