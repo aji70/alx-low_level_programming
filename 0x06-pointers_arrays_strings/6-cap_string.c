f6ec5ab471a575d4ced43ed430671032e7f694e4
Ajidokwu Sabo <realjaiboi70@gmail.com>
2022-12-22 17:13:45 -0800

    0x06

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,11 +1,14 @@
+#include "main.h"
+
 int check_seperators(char c);
 
 /**
  * cap_string - a function that capitalizes all words of a string
+ * @s: An input string to capitalize letters
  * Return: pointer to s
  */
 
-char *cap_string(char *)
+char *cap_string(char *s)
 {
 	int i = 0;
 
