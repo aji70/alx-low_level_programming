079aab9201cfa6a48e338562ddb75345de4d5cac
Ajidokwu Sabo <realjaiboi70@gmail.com>
2023-01-10 07:35:21 -0800

    0x0B

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -14,8 +14,8 @@ void free_grid(int **grid, int height)
 	int i = 0;
 
 	if (grid == NULL)
-		return
-			while (i < height)
-				free((int *)grid[i++]);
+		return;
+	while (i < height)
+		free((int *)grid[i++]);
 	free(grid);
 }
